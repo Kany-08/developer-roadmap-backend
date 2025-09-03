244ded56f7aa7a655934100a7eab8b812f6ab2ef
kany <kanysharapova@gmail.com>
2025-09-03 12:10:35 +0200

    feat: refactor server to use ES modules and add roadmap data

--- basic.js
+++ basic.js
@@ -1,9 +1,34 @@
-const express = require("express");
+import express from "express";
+import roadmap from "./data/roadmap.js";
+import cors from "cors";
+import { v4 } from "uuid";
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(cors());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(roadmap);
+});
+
+app.post("/", (req, res) => {
+  roadmap.push({
+    id: v4(),
+    ...req.body,
+  });
+  res.send(roadmap);
+});
+
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const index = roadmap.findIndex((item) => item.id === id);
+  if (index !== -1) {
+    roadmap.splice(index, 1);
+  }
+  res.send(roadmap);
 });
 
 app.listen(port, () => {
